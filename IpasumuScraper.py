d3bc64054328df3c7ffcf78d7bccc8c93d89d469
Kristaps_Dulbinskis_241RDB253 <kristaps.dulbinskis@edu.rtu.lv>
2025-05-14 01:50:28 +0300

    Update IpasumuScraper.py
    pēdējā koda versija
    
    implementēta privātmāju un lauku māju atbalsts
    saglabātais .xlsx fails norādīs arī kāds īpašums izvēlēts sākumā
    
    es apzinos savas kļudas par laika plānošanu

--- IpasumuScraper.py
+++ IpasumuScraper.py
@@ -7,11 +7,38 @@ headers = {
 }
 base_url = 'https://www.ss.lv'
 
+# Izvēlamies kuru kategoriju izvēlēties
+
+print("--- Pieejamie īpašumi ---")
+print("- Dzīvokļi")
+print("- Mājas un vasarnīcas")
+print("- Lauku viensētas")
+category = input("Izvēlies vienu no īpašumu tipiem: ")
+print()
+
+match category.lower():
+    case "dzīvokļi":
+        main_url = f'{base_url}/lv/real-estate/flats/sell/'
+        category = "flats"
+    case "mājas un vasarnīcas":
+        main_url = f'{base_url}/lv/real-estate/homes-summer-residences/sell/'
+        category = "homes"
+    case "lauku viensētas":
+        main_url = f'{base_url}/lv/real-estate/farms-estates/sell/'
+        category = "farms"
+    case _:
+        print("Nav tāds īpašuma tips")
+
 # Nosakām pilsētas ar rajoniem
-citieswithdistricts = ["rīga", "rīgas rajons", "daugavpils un rajons", "jelgava un rajons", "liepāja un rajons"]
+if category == "flats":
+    citieswithdistricts = ["rīga", "rīgas rajons", "daugavpils un rajons", "jelgava un rajons", "liepāja un rajons"]
+elif category == "homes":
+    citieswithdistricts = ["rīga", "rīgas rajons"]
+else:
+    citieswithdistricts = []
+    
 
 # Iegūstam pilsētu saites
-main_url = f'{base_url}/lv/real-estate/flats/sell/'
 response = requests.get(main_url, headers=headers)
 soup = BeautifulSoup(response.content, 'html.parser')
 
@@ -55,11 +82,11 @@ if user_city in city_map:
         user_district = input("\nIevadi rajonu no saraksta: ").strip().lower()
         if user_district == "visi":
             full_url = base_url + "all"
-            workbook = xlsxwriter.Workbook(user_city.replace(' ','-')+'-all.xlsx')
+            workbook = xlsxwriter.Workbook(category+'-'+user_city.replace(' ','-')+'-all.xlsx')
         elif user_district in districts:
             userdistrict = districts[user_district].replace(' ','-')
             full_url = base_url + districts[user_district]
-            workbook = xlsxwriter.Workbook(user_city.replace(' ','-')+"-"+user_district.replace(' ','-')+'.xlsx')
+            workbook = xlsxwriter.Workbook(category+'-'+user_city.replace(' ','-')+"-"+user_district.replace(' ','-')+'.xlsx')
         else:
             print("Rajons nav atrasts.")
             exit()
@@ -67,23 +94,40 @@ if user_city in city_map:
     else:
         # Parastajām pilsētām nav rajonu
         full_url = base_url + city_map[user_city]
-        workbook = xlsxwriter.Workbook(user_city.replace(' ','-')+'.xlsx')
+        workbook = xlsxwriter.Workbook(category+'-'+user_city.replace(' ','-')+'.xlsx')
 
     # Filtru ievadīšana
 
-    #istabu skaits
-    try:
-        min_rooms = int(input("Ievadi minimālo istabu skaitu(1-6): "))
-    except:
-        min_rooms = 0
-        print("Filtrs netiks ieskaitīts")
-    try:
-        max_rooms = int(input("Ievadi maksimālo istabu skaitu(1-6): "))
-    except:
-        max_rooms = 6
-        print("Filtrs netiks ieskaitīts")
+    # istabu skaits
+    min_rooms = 0   # lai nav kļudas
+    max_rooms = 10  # ^
+    if category == "flats":
+        try:
+            min_rooms = int(input("Ievadi minimālo istabu skaitu(1-6): "))
+        except:
+            min_rooms = 0
+            print("Filtrs netiks ieskaitīts")
+        try:
+            min_rooms = int(input("Ievadi maksimālo istabu skaitu(1-6): "))
+        except:
+            max_rooms = 10
+            print("Filtrs netiks ieskaitīts")
+        print()
 
-    print()
+    # stāvu skaits
+    min_floors = 0  # tā pat kā istabas
+    max_floors = 10 # ^
+    if category != "flats":
+        try:
+            min_floors = int(input("Ievadi minimālo stāvu skaitu: "))
+        except:
+            min_floors = 0
+            print("Filtrs netiks ieskaitīts")
+        try:
+            pass
+        except:
+            max_floors = 10
+            print("Filtrs netiks ieskaitīts")
 
     # platība
     try:
@@ -116,9 +160,10 @@ if user_city in city_map:
     # -----
     # lapu skaits
     try:
-       pages = int(input("Ievadi cik lapas nolasīt: "))
+        pages = int(input("Ievadi cik lapas nolasīt: "))
     except:
-       pages = 0
+        print("Filtrs netiks ieskaitīts")
+        pages = 1
 
     # -----
 
@@ -148,42 +193,131 @@ if user_city in city_map:
         soup = BeautifulSoup(response.content, 'html.parser')
         ad_listings = soup.find_all("tr", id=lambda x: x and x.startswith("tr_"))
 
-        for ad in ad_listings:
-            cells = ad.find_all('td')
-            if len(cells) >= 5:
-                try:
-                    location = cells[3].text.strip()
-                    room_info = int(cells[4].text.strip())
-                    # istabu skaits
-                    if room_info < min_rooms or room_info > max_rooms:
+        if category == "flats":
+            # izveido tabulu
+            worksheet = workbook.add_worksheet()
+            worksheet.write_row(0, 0, ["Atrašanās vieta", "Istabas", "Platība", "Cena (€)"])
+            row = 1
+
+            for ad in ad_listings:
+                cells = ad.find_all('td')
+                if len(cells) >= 5:
+                    try:
+                        location = cells[3].text.strip()
+                        room_info = int(cells[4].text.strip())
+                        # istabu skaits
+                        if room_info < min_rooms or room_info > max_rooms:
+                            continue
+                        # platiba
+                        size = int(cells[5].text.strip())
+                        if size < min_size or size > max_size:
+                            continue
+                        #cena
+                        price = int(cells[-1].text.strip().replace("  €","").replace(",",""))
+                        if price < min_price or price > max_price:
+                            continue
+
+                        print("Atrašanas vieta:", location)
+                        print("Istabas:", room_info)
+                        print("Platība:", size, "m2")
+                        print("Cena:", price,"€")
+                        print("------")
+
+                        matching += 1
+                        worksheet.write_row(row, 0, [location, room_info, size, price])
+                        row += 1
+
+                    except:
+                        print("Kļūda:")
                         continue
-                    # platiba
-                    size = int(cells[5].text.strip())
-                    if size < min_size or size > max_size:
+        
+            print("Ar kritērijiem sakrita "+str(matching)+"/"+str(len(ad_listings)+1))
+            currentpage +=1
+            matching = 0
+        elif category == "homes":
+            # izveido tabulu
+            worksheet = workbook.add_worksheet()
+            worksheet.write_row(0, 0, ["Atrašanās vieta", "Stāvi", "Platība", "Cena (€)"])
+            row = 1
+
+            for ad in ad_listings:
+                cells = ad.find_all('td')
+                if len(cells) >= 5:
+                    try:
+                        location = cells[3].text.strip()
+
+                        # stāvu skaits
+                        floors = int(cells[5].text.strip())
+                        if floors < min_floors or floors > max_floors:
+                            continue
+
+                        # platiba
+                        size = int(cells[4].text.strip())
+                        if size < min_size or size > max_size:
+                            continue
+                        #cena
+                        price = int(cells[-1].text.strip().replace("  €","").replace(",",""))
+                        if price < min_price or price > max_price:
+                            continue
+
+                        print("Atrašanas vieta:", location)
+                        print("Stāvi:", floors)
+                        print("Platība:", size, "m2")
+                        print("Cena:", price,"€")
+                        print("------")
+
+                        matching += 1
+                        worksheet.write_row(row, 0, [location, floors, size, price])
+                        row += 1
+
+                    except:
+                        print("Kļūda:")
                         continue
-                    #cena
-                    price = int(cells[-1].text.strip().replace("  €","").replace(",",""))
-                    if price < min_price or price > max_price:
+        
+            print("Ar kritērijiem sakrita "+str(matching)+"/"+str(len(ad_listings)+1))
+            currentpage +=1
+            matching = 0
+        else:
+            # izveido tabulu
+            worksheet = workbook.add_worksheet()
+            worksheet.write_row(0, 0, ["Atrašanās vieta", "Stāvi", "Platība", "Cena (€)"])
+            row = 1
+
+            for ad in ad_listings:
+                cells = ad.find_all('td')
+                if len(cells) >= 5:
+                    try:
+                        location = cells[3].text.strip()
+                        # stāvu skaits
+                        floors = int(cells[4].text.strip())
+                        # platiba
+                        size = int(cells[5].text.strip())
+                        if size < min_size or size > max_size:
+                            continue
+                        #cena
+                        price = int(cells[-1].text.strip().replace("  €","").replace(",",""))
+                        if price < min_price or price > max_price:
+                            continue
+
+                        print("Atrašanas vieta:", location)
+                        print("Stāvi:", floors)
+                        print("Platība:", size, "m2")
+                        print("Cena:", price,"€")
+                        print("------")
+
+                        matching += 1
+                        worksheet.write_row(row, 0, [location, floors, size, price])
+                        row += 1
+
+                    except:
+                        print("Kļūda:")
                         continue
-
-                    print("Atrašanas vieta:", location)
-                    print("Istabas:", room_info)
-                    print("Platība:", size, "m2")
-                    print("Cena:", price,"€")
-                    print("------")
-
-                    matching += 1
-                    worksheet.write_row(row, 0, [location, room_info, size, price])
-                    row += 1
-
-                except:
-                    print("Kļūda:")
-                    continue
-
-        currentpage +=1
+        
+            print("Ar kritērijiem sakrita "+str(matching)+"/"+str(len(ad_listings)+1))
+            currentpage +=1
+            matching = 0
 
     workbook.close()
-    print("Ar kritērijiem sakrita "+str(matching)+"/"+str(len(ad_listings)+1))
 
 else:
     print("Šada pilsēta nav atrasta sludinājumos.")
